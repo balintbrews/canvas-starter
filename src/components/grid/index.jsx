51239be85edbc647052364704e1617df3ce6c069
Balint Kleri <b@balintbrews.com>
2026-02-16 01:09:25 +0100

    feat(components:grid): add className prop

--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -1,6 +1,13 @@
-const Grid = ({ content }) => {
+import { cn } from "drupal-canvas";
+
+const Grid = ({ content, className }) => {
   return (
-    <div className="grid w-full min-w-sm grid-cols-1 gap-6 md:grid-cols-3 md:gap-8">
+    <div
+      className={cn(
+        "grid w-full min-w-sm grid-cols-1 gap-6 md:grid-cols-3 md:gap-8",
+        className,
+      )}
+    >
       {content}
     </div>
   );
